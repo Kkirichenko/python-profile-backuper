bfee03528d3fec6bbf8c363618277ccf431f90be
kkirichenko <postindustrialitycybershot@gmail.com>
2019-11-15 04:26:33 +0300

    some PEP8

--- backuper.py
+++ backuper.py
@@ -5,7 +5,8 @@ import time
 import sys
 import zipfile
 
-# bash 
+
+# bash
 def bash(command):
     return os.popen(command).read().strip()
 
@@ -15,19 +16,21 @@ FAIL_RED = '\033[91m'
 
 # variables
 script_path = os.path.dirname(os.path.realpath(__file__))
-name = 'portablework_'+time.strftime('%d%m%Y')+'.zip' # archive name
-wsl_user = bash('whoami') # wsl linux user
-win_user = bash('cmd.exe /c "echo %USERNAME%" 2>/dev/null') # windows user 
+name = 'portablework_'+time.strftime('%d%m%Y')+'.zip'  # archive name
+wsl_user = bash('whoami')  # wsl linux user
+win_user = bash('cmd.exe /c "echo %USERNAME%" 2>/dev/null')  # windows user
 # source
-homedir = bash('echo ~') # wsl home dir
-conemu = '/mnt/c/Users/'+win_user+'/AppData/Roaming/ConEmu.xml' # CenEmu config 
+homedir = bash('echo ~')  # wsl home dir
+conemu = '/mnt/c/Users/'+win_user+'/AppData/Roaming/ConEmu.xml'  # CenEmu conf
 vimrc = '/etc/vim/vimrc'
-sshconf = '/etc/ssh/ssh_config' 
+sshconf = '/etc/ssh/ssh_config'
 # files source list
 f_source = [conemu, vimrc, sshconf]
+
+
 # backup
 def backup():
-# open archive
+    # open archive
     print('creating archive...')
     z = zipfile.ZipFile(name, 'w')
 # backup homedir
@@ -41,10 +44,12 @@ def backup():
         z.write(file)
     z.close()
 
+
 def upload():
-    os.system('gdrive upload '+name) 
+    os.system('gdrive upload '+name)
     os.system('rm '+name)
 
 backup()
 upload()
 print(OK_GREEN+'DONE!'+OK_GREEN)
+
