fe5ab236d72dd1a34840514fb35e2df094a06c6e
kkirichenko <postindustrialitycybershot@gmail.com>
2019-11-25 02:34:56 +0300

    reforge

---
+++ README.md
@@ -0,0 +1,16 @@
+# python-profile-backuper
+Simple python script for backup homedir and some config files to google drive
+
+## Usage: 
+
+if need to add files - edit f_source list in script (user homedir backup as default)
+
+```
+-b to backup 
+
+-r to restore
+```
+## Built With
+
+* [Gdrive](https://github.com/gdrive-org/gdrive) - command line utility for interacting with Google Drive
+* [zipfile](https://docs.python.org/3/library/zipfile.html#module-zipfile) - python zip module
--- backuper.py
+++ backuper.py
@@ -1,84 +1,53 @@
 #!/usr/bin/env python3
+
 import os
-import re
 import time
-import sys
 import zipfile
-from os.path import basename
+import sys
 
 
-# bash
-def bash(cmd):
-    return os.popen(cmd).read().strip()
 
-# warn colors
-OK_GREEN = '\033[92m'
-FAIL_RED = '\033[91m'
+homedir = os.getenv('HOME')
+name = 'work_backup_'+time.strftime('%d%m%Y')+'.zip'  # archive name
+id_archive = os.popen("gdrive list | grep work | awk '{print$1}'").read().strip()
 
-# variables
-script_path = os.path.dirname(os.path.realpath(__file__))
-name = 'portablework_'+time.strftime('%d%m%Y')+'.zip'  # archive name
-wsl_user = bash('whoami')  # wsl linux user
-win_user = bash('cmd.exe /c "echo %USERNAME%" 2>/dev/null')  # windows user
-# source
-homedir = bash('echo ~')  # ws home dir
-conemu = '/mnt/c/Users/'+win_user+'/AppData/Roaming/ConEmu.xml'  # CenEmu conf
-vimrc = '/etc/vim/vimrc'
-sshconf = '/etc/ssh/ssh_config'
-# files source list
-f_source = [conemu, vimrc, sshconf]
-
-
-# backup
 def backup():
-    # open archive
     print('creating archive...')
     z = zipfile.ZipFile(name, 'w')
     print('backup homedir...')
-# backup homedir
-    for root, dirs, files in os.walk(homedir):
+    for root, dirs, files in os.walk(os.getenv('HOME')):
         for file in files:
             if name in files:
-                files.remove(name)
+               files.remove(name)
+            if '.vim' in dirs:
+                dirs.remove('.vim')
+            if '.git' in dirs:
+                dirs.remove('.git')
             z.write(os.path.join(root, file))
-# backup other stuff
-    if conemu in f_source:
-        f_source.remove(conemu)
-        z.write(conemu, basename(conemu))
-    for file in f_source:
-        z.write(file)
-    print('backup some stuff')
     z.close()
 
-
-# upload then delete archive
 def upload(archive):
-    print('uploading archive...')
-    bash('gdrive upload '+archive)
+    print('delete last cloud archive')
+    os.system('gdrive delete ' + id_archive)
+    print('upload archive...')
+    os.system('gdrive upload ' + archive)
     print('delete local archive')
-    bash('rm '+archive)
-    print(OK_GREEN+'DONE!'+OK_GREEN)
+    os.remove(archive)
+    print('\033[92mDONE!\033[92m')
 
-
-# download archive
 def download():
-    print('Download archive...')
-    bash("gdrive download `gdrive list | grep work | sort -rk 2,2 | awk 'NR==1{print$1}'`") 
-    return bash("gdrive list | grep work | sort -rk 2,2 | awk 'NR==1{print$2}'")
+    print('download archive...')
+    os.system("gdrive download " + id_archive)
+    return os.popen("gdrive list | grep work | awk '{print$2}'").read().strip()
 
-
-# exctract all from archive
 def extract(archive):
     z = zipfile.ZipFile(archive, 'r')
     print('extract archive...')
     z.extractall('/')
     z.close()
-    print('delete local archive')
-    bash('rm '+archive)
-    
+    print('delete downloaded archive')
+    os.remove(archive)
 
-
-# keys
 if sys.argv[1] == '-b':
     backup()
     upload(name)
