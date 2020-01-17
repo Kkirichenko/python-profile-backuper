d259768e21d65093962a2caff52c0b83ea904d1a
kkirichenko <postindustrialitycybershot@gmail.com>
2020-01-17 23:18:56 +0300

    some print fix

--- backuper.py
+++ backuper.py
@@ -13,9 +13,10 @@ id_archive = os.popen(
 
 
 def backup():
-    print('creating archive...')
+    print('creating archive', end=' ', flush='True')
     z = zipfile.ZipFile(name, 'w')
-    print('backup homedir...')
+    print('✓')
+    print('backup homedir',end=' ', flush='True')
     for root, dirs, files in os.walk(os.getenv('HOME')):
         for file in files:
             if name in files:
@@ -26,41 +27,47 @@ def backup():
                 dirs.remove('.git')
             z.write(os.path.join(root, file))
     z.close()
+    print('✓')
 
 
 def upload(archive):
     print('delete last cloud archive')
     os.system('gdrive delete ' + id_archive)
-    print('upload archive...')
+    print('upload archive')
     os.system('gdrive upload ' + archive)
-    print('delete local archive')
+    print('delete local archive', end=' ', flush='True')
     os.remove(archive)
-    print('\033[92mDONE!\033[92m')
+    print('✓')
 
 
 def download():
-    print('download archive...')
+    print('download archive', end=' ', flush='True')
     os.system("gdrive download " + id_archive)
     return os.popen("gdrive list | grep work | awk '{print$2}'").read().strip()
+    print('✓')
 
 
 def extract(archive):
     z = zipfile.ZipFile(archive, 'r')
-    print('extract archive...')
+    print('extract archive', end=' ', flush='True')
     z.extractall('/')
     z.close()
-    print('delete downloaded archive')
+    print('✓')
+    print('delete downloaded archive', end=' ', flush='True')
     os.remove(archive)
+    print('✓')
 
 
 parser = argparse.ArgumentParser(
     description='backup/restore homedir into/from google drive')
-parser.add_argument('--backup', action='store_true', help='backup homedir to gdrive')
-parser.add_argument('--restore', action='store_true', help='restore homedir from gdrive')
+parser.add_argument('--backup', '-b', action='store_true', help='backup homedir to gdrive')
+parser.add_argument('--restore', '-r', action='store_true', help='restore homedir from gdrive')
 args = parser.parse_args()
 
 if args.backup:
     backup()
     upload(name)
+    print('\033[92mBACKUP DONE!\033[92m')
 elif args.restore:
     extract(download())
+    print('\033[92mRESTORE DONE!\033[92m')
